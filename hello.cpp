c464f574b88f45129fe01aebd8b3cbf6f9018300
Braian <braianoquen@gmail.com>
2026-02-10 16:28:31 -0500

    first commit

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+// par o impar
+
+int num; //inicializo la variable num
+cout << "Ingrese un numero: " << endl;
+cin >> num;
+if(num % 2 == 0){
+    cout << "el numero:"<< num<< " es par" << endl;
+}
+
+    
+    return 0;
+}
